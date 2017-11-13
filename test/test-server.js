f92103668162e14505b2f11908066bff9ef47cf7
rk-codes <ping.you.back@gmail.com>
2017-11-13 00:14:11 -0800

    add tests for recipes

--- test/test-server.js
+++ test/test-server.js
@@ -138,3 +138,65 @@ describe('Shopping List', function() {
       });
   });
 });
+
+describe('Recipes', function() {
+  it('should list items on GET', function() {
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      res.body.length.should.be.at.least(2);
+      const expectedKeys = ['id', 'name', 'ingredients'];
+        res.body.forEach(function(item) {
+          item.should.be.a('object');
+          item.should.include.keys(expectedKeys);
+        });
+    });
+  });
+
+  it('should add an item on POST', function() {
+    const recipe = {name : 'coffee', ingredients : ['milk', 'coffee', 'sugar']};
+    return chai.request(app)
+    .post('/recipes')
+    .send(recipe)
+    .then(function(res) {
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.include.keys('id', 'name', 'ingredients');
+      res.body.id.should.not.be.null;
+    })
+  });
+
+  it('should delete items on DELETE', function() {
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      return chai.request(app)
+        .delete(`/recipes/${res.body[0].id}`);
+    })
+    .then(function(res) {
+      res.should.have.status(204);
+    });
+  });
+
+  it('should update items on PUT', function() {
+    const updateData = {
+      name: 'coffee',
+      ingredients: ['water', 'sugar', 'coffee']
+    };
+    return chai.request(app)
+    .get('/recipes')
+    .then(function(res) {
+      updateData.id = res.body[0].id;
+      return chai.request(app)
+        .put(`/recipes/${updateData.id}`)
+        .send(updateData)
+    })
+    .then(function(res) {
+       res.should.have.status(204);
+     });
+  }); 
+});
